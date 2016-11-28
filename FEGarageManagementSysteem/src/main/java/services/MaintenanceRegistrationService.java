7ce7a044609c4c8266b922a3efab34547f0d7232
paisan <paisan.rietbroek@gmail.com>
2016-11-28 14:25:44 +0100

    flowscoped still broke

--- FEGarageManagementSysteem/src/main/java/services/MaintenanceRegistrationService.java
+++ FEGarageManagementSysteem/src/main/java/services/MaintenanceRegistrationService.java
@@ -1,7 +1,9 @@
 package services;
 
+import entities.Car;
 import lombok.Data;
 
+import javax.ejb.EJB;
 import javax.faces.flow.FlowScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -12,7 +14,20 @@ import java.io.Serializable;
 
 @Data
 @Named("maintenanceRegistrationService")
-@FlowScoped("maintenance_registratoin")
+@FlowScoped("maintenance_registration")
 public class MaintenanceRegistrationService implements Serializable {
+
+    @EJB
+    private ICarService carService;
+
+    @EJB ICustomerService customerService;
+
+    private Car car;
+
+    private String description;
+
+    public MaintenanceRegistrationService() {
+        this.car = new Car();
+    }
 
 }
