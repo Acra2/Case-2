13920e9ec3b288d27d2978df4ecab24e7378295f
paisan <paisan.rietbroek@gmail.com>
2016-11-28 12:38:02 +0100

    pff

--- BSVoertuigEnKlantbeheer/src/main/java/setup/SetMaintenanceData.java
+++ BSVoertuigEnKlantbeheer/src/main/java/setup/SetMaintenanceData.java
@@ -10,6 +10,7 @@ import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -17,7 +18,7 @@ import java.time.LocalDateTime;
  */
 @Singleton
 @Startup
-public class SetMaintenanceData {
+public class SetMaintenanceData implements Serializable {
     @EJB
     private ICarService carService;
 
